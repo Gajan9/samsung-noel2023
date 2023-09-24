a6466be93621b2250b9bec15e6a9dead6bd748e4
Gajan9 <baskaran.gajan@heaven.fr>
2023-09-24 22:24:54 +0200

    ajout caroussel

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+console.log("hello");
+
+setInterval("deplace()", 10);
+nbImage = 4;
+largeurImage = 640;
+positionCourante = new Array(nbImage);
+
+for (i = 0; i < nbImage; i++) {
+    positionCourante [i] = i * largeurImage;
+}
+
+function deplace() {
+    for (i=0; i < nbImage; i++) {
+        positionCourante[i] -= 1;
+
+        if (positionCourante[i] < -largeurImage) {
+            positionCourante[i] = (nbImage - 1) * largeurImage;
+        }
+        document.getElementById("image" + (i+1)).style.left = positionCourante[i] + "px";
+    }
+}
